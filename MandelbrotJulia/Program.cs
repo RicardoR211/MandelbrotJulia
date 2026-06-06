e187ae9e66895986af0c5de213c4b81125cdbbf9
Ricardo <65051149+RicardoR211@users.noreply.github.com>
2026-06-06 16:00:53 -0300

    Adicionar arquivos de projeto.

---
+++ MandelbrotJulia/Program.cs
@@ -0,0 +1,30 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MandelbrotJulia
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Raylib.InitWindow(800, 480, "MandelbrotJulia");
+            Raylib.SetTargetFPS(60);
+
+            //Loop principal do jogo
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                Raylib.DrawText("Primeiro Commit", 190, 200, 20, Color.LightGray);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+
+        }
+    }
+}
